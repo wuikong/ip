33e5a3026bbc604726ffd2f2876fc4ac66c62154
wuikong <126385630+wuikong@users.noreply.github.com>
2026-01-23 13:43:47 +0800

    Add taskList

--- src/main/java/Catbot.java
+++ src/main/java/Catbot.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Catbot {
+    private static ArrayList<String> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Catbot\nWhat can I do for you?");
