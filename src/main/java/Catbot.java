55cceabca5ff276d2f4607929cabdbae676e39ef
wuikong <126385630+wuikong@users.noreply.github.com>
2026-01-23 13:34:56 +0800

    Import scanner

--- src/main/java/Catbot.java
+++ src/main/java/Catbot.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Catbot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Catbot\nWhat can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
