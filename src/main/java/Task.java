fe15ede245df0c5edb3c564aa0824b3c8e2784c6
wuikong <126385630+wuikong@users.noreply.github.com>
2026-01-23 13:58:27 +0800

    Add isDone to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,23 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
     }
 
     @Override
     public String toString() {
-        return description;
+        String checkbox = "[" + (isDone ? "X" : " ") + "] ";
+        return checkbox + description;
     }
 }
