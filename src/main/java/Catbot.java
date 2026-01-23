bb6d705f6683e7b58bca3af8eb57663a212b6931
wuikong <126385630+wuikong@users.noreply.github.com>
2026-01-23 13:38:33 +0800

    Add command entry loop

--- src/main/java/Catbot.java
+++ src/main/java/Catbot.java
@@ -4,6 +4,14 @@ public class Catbot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Catbot\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
