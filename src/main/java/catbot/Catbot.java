9f637ba117b50f6caf04110fe503d3afca40ec30
wuikong <126385630+wuikong@users.noreply.github.com>
2026-02-06 15:43:19 +0800

    Add GUI components from JavaFX tutorial

--- src/main/java/catbot/Catbot.java
+++ src/main/java/catbot/Catbot.java
@@ -116,4 +116,11 @@ public class Catbot {
             }
         }
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
