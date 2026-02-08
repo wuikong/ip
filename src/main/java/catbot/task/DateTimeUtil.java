8d7557ebe1799ab5204b766b49b7f537e0d5960c
wuikong <126385630+wuikong@users.noreply.github.com>
2026-02-09 00:25:36 +0800

    Make DateTimeUtil final

--- src/main/java/catbot/task/DateTimeUtil.java
+++ src/main/java/catbot/task/DateTimeUtil.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * Provides shared datetime formatting utilities.
  */
-public class DateTimeUtil {
+public final class DateTimeUtil {
     public static final String INPUT_PATTERN = "yyyy-MM-dd HHmm";
     public static final String OUTPUT_PATTERN = "MMM d yyyy HHmm";
     public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern(INPUT_PATTERN);
